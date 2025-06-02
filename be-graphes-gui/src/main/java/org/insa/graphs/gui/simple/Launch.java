c9717acc6e8d8d6988a03c8dc6c0b287e55e7f93
AnnaLisa18 <josseanna3@gmail.com>
2025-06-03 00:25:20 +0200

    update test

--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -23,6 +23,7 @@ import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
 import org.insa.graphs.algorithm.shortestpath.ShortestPathSolution;
 import org.insa.graphs.algorithm.shortestpath.BellmanFordAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.DijkstraAlgorithm;
+import org.insa.graphs.algorithm.shortestpath.AStarAlgorithm;
 
 public class Launch {
 
@@ -85,13 +86,17 @@ public class Launch {
             drawing.drawGraph(graph);
             
             // Test automatique
-            // on teste plusieurs scénarios { origine, destination, 0 si distance OU 1 si temps}
+            // on teste plusieurs scénarios { origine, destination, 0 si distance OU 1 si temps, 0 toutes routes 1 si only voiture}
 
             int[][] scenarios={
-                {1,40000,0}, // test du plus court chemin au niveau de la distance
-                {100,80000,1},// test du plus court chemin au niveau du temps
-                {30,30,0}, //test pour scenario avec origine=arrivee
-                {1,1000000,0}, //test pour scenarios avec mauvais indices
+                {1,40000,0,0}, // test du plus court chemin au niveau de la distance
+                {100,80000,1,0},// test du plus court chemin au niveau du temps
+                {30,30,0,0}, //test pour scenario avec origine=arrivee
+                {1,1000000,0,0}, //test pour scenarios avec mauvais indices
+                {7232,29607,0,0}, // test chemin plus court, toute les routes
+                {7232,29607,0,1}, // test chemin plus court, que voiture
+                {7232,29607,1,0}, // test chemin plus rapide, toute les routes
+                {7232,29607,0,0}, // test chemin plus rapide, que les piétons
             };
 
             int TotalTest=scenarios.length;
@@ -101,6 +106,7 @@ public class Launch {
                 int origine = scenario[0];
                 int destination= scenario[1];
                 int test= scenario[2];
+                int voiture = scenario[3];
 
                 //Test pour vérifier de pas etre index out of range
                 if(origine>= graph.size() || destination >= graph.size()){
@@ -110,9 +116,9 @@ public class Launch {
 
                 }
                 
-
-                // savoir si t'utilise le filtre du temps ou de la distance
-                ArcInspector inspector = (test == 0) ?ArcInspectorFactory.getAllFilters().get(0) : ArcInspectorFactory.getAllFilters().get(2);
+                // savoir si t'utilise le filtre du temps ou de la distance  et/ou mode voiture ou toute les routes
+                int indexFiltre = (test==0 ? 0:2) + (voiture == 1 ? 1 : 0);
+                ArcInspector inspector = ArcInspectorFactory.getAllFilters().get(indexFiltre);
 
                 // Je crée mon objet ShortestPathData qui va être en donnée pour l'algo djikstra
                 // il contien ( graph travail, noeud origin, noeud dest, type test distance ou temps)
@@ -128,25 +134,34 @@ public class Launch {
                 BellmanFordAlgorithm bellman = new BellmanFordAlgorithm(data);
                 ShortestPathSolution bellmansol = bellman.run();
 
+                //A star
+                AStarAlgorithm astar = new AStarAlgorithm(data);
+                ShortestPathSolution astarsol = astar.run();
+
+                
+                
                 // Affichage
                 System.out.println("Scénario"+ origine+ "->" +destination+"(" + (test==0 ? "distance" : "temps")+")");
                 
-                boolean DeuxFaisable = dijkstrasol.isFeasible() && bellmansol.isFeasible();
-                boolean DeuxPasFaisable= !dijkstrasol.isFeasible() && !bellmansol.isFeasible();
+                boolean tousFaisables = dijkstrasol != null && dijkstrasol.isFeasible() && bellmansol != null && bellmansol.isFeasible() && astarsol != null && astarsol.isFeasible();
+                boolean tousInfeasibles= (dijkstrasol == null || !dijkstrasol.isFeasible()) && (bellmansol == null || !bellmansol.isFeasible()) && (astarsol==null || !astarsol.isFeasible());
+                
 
-                if(DeuxFaisable){
+                if(tousFaisables){
                     Path chemin_1 = dijkstrasol.getPath();
                     Path chemin_2 =bellmansol.getPath();
+                    Path chemin_3 = astarsol.getPath();
 
                     double marge_erreur= 0.01; // j'avais pris 1e-3 mais trop strict pour la map carre dense
 
                     // on va vérifier ou non si dijkstra et bellman ford on plus ou moins les même resultats ou pas
-                    boolean MemeLongueur = Math.abs(chemin_1.getLength() - chemin_2.getLength()) < marge_erreur;
-                    boolean MemeTemps = Math.abs(chemin_1.getMinimumTravelTime() - chemin_2.getMinimumTravelTime()) < marge_erreur;
+                    boolean MemeLongueur = Math.abs(chemin_1.getLength() - chemin_2.getLength()) < marge_erreur && Math.abs(chemin_1.getLength()-chemin_3.getLength()) < marge_erreur;
+                    boolean MemeTemps = Math.abs(chemin_1.getMinimumTravelTime() - chemin_2.getMinimumTravelTime()) < marge_erreur && Math.abs(chemin_1.getMinimumTravelTime() - chemin_3.getMinimumTravelTime())< marge_erreur;
 
                     // Affichage
                     System.out.printf(" Dijkstra: %.2f m, %.2f s%n", chemin_1.getLength(),chemin_1.getMinimumTravelTime());
                     System.out.printf(" Bellman: %.2f m, %.2f s%n", chemin_2.getLength(),chemin_2.getMinimumTravelTime());
+                    System.out.printf(" A*: %.2f m, %.2f s%n", chemin_3.getLength(),chemin_3.getMinimumTravelTime());
                     System.out.printf("                                                                                 %n");
 
                 
@@ -156,14 +171,14 @@ public class Launch {
                         System.out.println(" Test Reussi");
                         System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
                         TestSucces++;
-                        drawing.drawPath(chemin_1 );
+                        drawing.drawPath(chemin_1);
 
                     }else{
                         System.out.println("test Echoué: resultats differents");
                         System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
                     }
                 }
-                else if(DeuxPasFaisable){
+                else if(tousInfeasibles){
                     System.out.println("Pas de chemin trouvé");
                     System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
                     TestSucces++;
